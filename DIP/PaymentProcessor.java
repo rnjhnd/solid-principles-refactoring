96ff80a2b6d35901498b4adc175c4225a5d678e3
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2025-03-23 01:20:52 +0000

    Update Files

--- DIP/PaymentProcessor.java
+++ DIP/PaymentProcessor.java
@@ -11,3 +11,4 @@ public class PaymentProcessor {
         paymentMethod.pay(amount);
     }
 }
+    
--- ISP/PhoneDemo.java
+++ ISP/PhoneDemo.java
@@ -5,13 +5,13 @@ public class PhoneDemo {
         BasicPhone basicPhone = new BasicPhone();
         System.out.println("----- Basic Phone -----");
         basicPhone.makeCall("0917 123 4567");
-        basicPhone.sendSMS("0927 456 1234", "Hello, This message is sent from a Basic Phone!");
+        basicPhone.sendSMS("0927 456 1234", "Hello, This message was sent from a Basic Phone!");
         System.out.println();
 
         Samsung samsung = new Samsung();
         System.out.println("----- Samsung -----");
         samsung.makeCall("0938 654 0987");
-        samsung.sendSMS("0949 321 5678", "Hello, This message is sent from a Samsung Phone!");
+        samsung.sendSMS("0949 321 5678", "Hello, This message was sent from a Samsung Phone!");
         samsung.browseWeb("https://www.samsung.com");
         samsung.takePicture();
         System.out.println();
@@ -19,7 +19,7 @@ public class PhoneDemo {
         IPhone iPhone = new IPhone();
         System.out.println("----- IPhone -----");
         iPhone.makeCall("0998 246 8102");
-        iPhone.sendSMS("0945 678 9101", "Hello, This message is sent from an iPhone!");
+        iPhone.sendSMS("0945 678 9101", "Hello, This message was sent from an iPhone!");
         iPhone.browseWeb("https://www.apple.com");
         iPhone.takePicture();
 
